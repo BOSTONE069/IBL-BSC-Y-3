cf084e6a5f197e5063a771c7b5f711f97fee5a51
BOSTONE069 <bostoneochieng69@gmail.com>
2023-04-04 14:37:21 -0400

    This is python programming for IBL

---
+++ functions.py
@@ -0,0 +1,28 @@
+#This is the print function
+"""This is the print function"""
+print("Hello, World!")
+print(2)
+print(3.4)
+
+
+#variables
+x = 2
+y = 3
+print(x)
+print(y)
+print(x + y)
+print(x * y)
+
+name = input("What is your name?")
+print(name)
+
+first_number = int(input("What is your first"))
+second_number = int(input("What is your second"))
+print(first_number * second_number)
+
+
+number_one = int(input("What is your first")) # this is
+number_two = int(input("What is your second"))
+final_number = number_one + number_two
+print(f"{final_number}") #this is 
+print("{} + {} = {}".format(number_one, number_two, final_number))
