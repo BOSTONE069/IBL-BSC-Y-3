a8eeec9cf1f1d0869e9d8c9a3db0f4fbd45a76aa
BOSTONE069 <bostoneochieng69@gmail.com>
2023-04-05 11:35:09 -0400

    indoor program for management of strings

---
+++ indoor.py
@@ -0,0 +1,16 @@
+# Prompt the user for input and store the result in a variable called input_str
+user_text = input("Please enter a string: ")
+
+# Convert the input string to lowercase and store the result in a variable called lowercase_str
+lowercase_str = user_text.lower()
+
+lowercase_str2 = user_text.capitalize()
+
+# Output the lowercase string to the console
+print(lowercase_str)
+
+#Return a copy of the string with its first character capitalized and the rest lowercased.
+print(lowercase_str2)
+
+#converting string to upper case
+print(lowercase_str2.upper())
