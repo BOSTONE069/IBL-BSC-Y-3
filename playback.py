84e9576aa74ba9d7d83a448dd220df786bae118e
BOSTONE069 <bostoneochieng69@gmail.com>
2023-04-05 14:34:43 -0400

    [200~Replace all occurrences of a space character with three periods and store the result in a variable called output_str~

---
+++ playback.py
@@ -0,0 +1,8 @@
+# Prompt the user for input and store the result in a variable called input_str
+user_text = input("Please enter a string: ")
+
+# Replace all occurrences of a space character with three periods and store the result in a variable called output_str
+output_str = user_text.replace(" ", "...")
+
+# Output the modified string to the console
+print(output_str)
