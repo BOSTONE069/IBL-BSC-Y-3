3906a9028c9ce9bfaac5537e8ede1ab4a71d7b1a
BOSTONE069 <bostoneochieng69@gmail.com>
2023-04-08 11:05:57 -0400

    This is the dictionaries in python

---
+++ dictionaries.py
@@ -0,0 +1,14 @@
+#student = ["Kevin", "Sheldon", "Brian", "John", "Joy"]
+estates = ["Kasarani", "Exit 9", "Syokimau", "Kayole", "Ngara"]
+
+students = {
+    "Kevin":"Kasarani",
+    "Sheldon":"Exit 9",
+    "Brain":"Syokimau",
+    "John":"Kayole",
+    "Joy":"Ngara"
+}
+
+for student in students:
+    print (student, students[student], sep=", ")
+
