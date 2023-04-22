e0612c6aeab325d3ebe9dee1dd0c8170c9fb0a84
BOSTONE069 <bostoneochieng69@gmail.com>
2023-04-22 19:05:23 -0400

    This is on classes setter and getters

--- login.py
+++ login.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 class Login():
     def __init__(self, username, password=None):
         self.username = username
@@ -14,7 +15,12 @@ class User(Login):
         return f"Your name is {self.first_name} {self.last_name} and your username is {self.username} and password is {self.password} and the email address is {self.email_address}"
 
 def  main():
-    user = User("Bostone001", "TUK001", "Ochieng", "Kevin", "bostone@gmail.com")
+    user_name = input("Please enter your username ")
+    pass_word = input("Please enter your password ")
+    first_name = input("Please enter your first name ")
+    last_name = input("Please enter your last name ")
+    email_address = input("Please enter your email address ")
+    user = User(user_name, pass_word, first_name, last_name, email_address)
     print(user)
 
 if __name__ == '__main__':
